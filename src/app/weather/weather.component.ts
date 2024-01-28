b3fa7dcb5b2edad374e0d2c1ac53cacb129348a4
TheAjinkya <ajinkya.chanshetty@gmail.com>
2024-01-28 00:36:07 +0000

    Updated the timezone

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -12,6 +12,11 @@ export class WeatherComponent implements OnInit {
 
   weatherData!: any
   localTime:any
+  currentTemp:any
+  feels_like:any
+  minTemp:any
+  maxTemp:any
+  today: number = Date.now();
 
   constructor(private weatherService: WeatherService) { }
 
@@ -19,15 +24,34 @@ export class WeatherComponent implements OnInit {
     this.weatherService.getData().subscribe(res=>{
       console.log("weatherService.getData", res)
       this.weatherData = res
+      this.setAllData(res)
     })
   }
 
   checkWeather(location:string){
-    this.weatherService.getData(location).subscribe(res=>{
+    this.weatherService.getData(location).subscribe((res:any)=>{
       console.log("weatherService.getData", res)
       this.weatherData = res
-      this.localTime = new Date(this.weatherData.dt)
+      this.setAllData(res)    
     })
+  }
+
+  changeLocation(location:any){
+    this.checkWeather(location)
+  }
+
+  setAllData(allWeather:any){
+    this.localTime = new Date(allWeather.dt)
+      this.currentTemp = this.kelvinToCelcius(allWeather.main.temp)
+      this.feels_like = this.kelvinToCelcius(allWeather.main.feels_like)
+      this.maxTemp = this.kelvinToCelcius(allWeather.main.temp_max)
+      this.minTemp = this.kelvinToCelcius(allWeather.main.temp_min)
+      
+      console.log(this.localTime)
+  }
+
+  kelvinToCelcius(temp:any){
+    return Math.round(temp -273)
   }
 
 }
