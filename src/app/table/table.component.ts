18fc21e026dbaf78db90f6bdd9747c65e88fada4
TheAjinkya <ajinkya.chanshetty@gmail.com>
2024-02-19 11:22:20 +0000

    added the table comp

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
